d4abbc71126d7b1ff6cb56f815db0cd82e17f8c1
Dinesh-Stack97 <68021533+Dinesh-Stack97@users.noreply.github.com>
2021-11-26 20:30:16 +0530

    Add files via upload

---
+++ one_TO_Many_std_laptop2_bidirectional/src/main/java/com/entity_creation/client.java
@@ -0,0 +1,55 @@
+package com.entity_creation;
+import java.io.Serializable;
+import java.lang.module.Configuration;
+import java.util.Date;
+
+import org.hibernate.Session;
+
+
+import com.entity_creation.entities.Laptop;
+
+import com.entity_creation.entities.Student;
+public class client {
+	
+	
+
+	
+	    public static void main( String[] args )
+	    {
+	     try(
+	    		 
+	    		 
+	    		 Session session =    ( Hibernate_session_factry.geSessionFactory()).openSession()){
+	    	 
+	    	
+	    
+         
+	    	session.beginTransaction();
+	    
+	    	
+	    	//session.persist(priest);//ITS RETURN TYPE IS VOID
+	    	
+	    	
+	           Laptop laptop = new Laptop();
+	           laptop.setLid(1);
+	           laptop.setLname("Dell");
+	           
+	           Student student = new Student();
+	           student.setRollno(1);
+	           student.setName("Dinesh");
+	           student.setMarks(60);
+	           student.setLaptop(laptop);
+	    	
+	            session.save(student);
+	            session.save(laptop);
+	            session.getTransaction().commit();
+	    }
+	     
+		 catch(Exception e)
+	     {
+	    	 e.printStackTrace();
+	     }
+	    }
+	    
+	
+}
